3c369b60295b06b880ff5c5b24e99d4b1c417687
eslam <eslamfgazy600@gmail.com>
2022-02-12 17:47:25 +0200

    ChatTolk

---
+++ app/src/main/java/com/multilingual/firebase/chat/activities/UIApplication.java
@@ -0,0 +1,17 @@
+package com.multilingual.firebase.chat.activities;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.multilingual.firebase.chat.activities.fcm.ApplicationLifecycleManager;
+import com.google.android.gms.ads.MobileAds;
+
+public class UIApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(this);
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        registerActivityLifecycleCallbacks(new ApplicationLifecycleManager());
+    }
+}
